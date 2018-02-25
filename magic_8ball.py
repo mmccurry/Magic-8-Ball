4bdef9158bc7ffb5028c6c0b260b9055190e13eb
Matthew  McCurry <mmccur3@gmail.com>
2018-02-25 15:42:15 -0600

    Add files via upload

--- magic_8ball.py
+++ magic_8ball.py
@@ -5,9 +5,9 @@ print("Welcome to the all-knowing magic 8-ball program.")
 while True:	
 	
 	responses = ["It is certain.", "As I see it, yes.", "Reply hazy. Try again.", "Don't count on it.", 
-	"It is decidedly so.", "Most likely.", "Most likely.", "Ask again later.", "My reply is no.", "Without a doubt.", 
+	"It is decidedly so.", "Most likely.", "Ask again later.", "My reply is no.", "Without a doubt.", 
 	"Outlook good.", "Better not tell you now.", "My sources say no.", "Yes, definitely.", "Yes", "Cannot predict now.", 
 	"Outlook not so good.", "You may rely on it.", "Signs point to yes.", "Concentrate and ask again.", "Very doubtful."]
 	
 	input("Press enter to shake the ball or control-c to quit.")
-	print(responses[random.randint(0,20)])
+	print(responses[random.randint(0,19)])
