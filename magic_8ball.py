a1ea9d7845ab86a2f164ca6856df10a8646a7f01
Matthew  McCurry <mmccur3@gmail.com>
2018-07-18 16:23:28 -0500

    Add files via upload

--- magic_8ball.py
+++ magic_8ball.py
@@ -1,13 +1,27 @@
 import random
+import os
 
-print("Welcome to the all-knowing magic 8-ball program.")
-
-while True:	
-	
+def screen():
 	responses = ["It is certain.", "As I see it, yes.", "Reply hazy. Try again.", "Don't count on it.", 
 	"It is decidedly so.", "Most likely.", "Ask again later.", "My reply is no.", "Without a doubt.", 
 	"Outlook good.", "Better not tell you now.", "My sources say no.", "Yes, definitely.", "Yes", "Cannot predict now.", 
 	"Outlook not so good.", "You may rely on it.", "Signs point to yes.", "Concentrate and ask again.", "Very doubtful."]
 	
-	input("Press enter to shake the ball or control-c to quit.")
-	print(responses[random.randint(0,19)])
+	os.system('clear')
+	print('''
+		-----------------------------------------------------
+					Magic 8-Ball
+		-----------------------------------------------------
+		''')
+	print("                " + responses[random.randint(0,19)])
+	print("\n\n\n\n               Press enter to shake ball. Press q to quit.")
+	choice = input('> ')
+
+	if choice == 'q':
+		os.system('clear')
+		exit(0)
+	else:
+		screen()	
+
+
+screen()
